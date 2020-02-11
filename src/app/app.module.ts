adf8222ec2cb12678efad1e4a6f11a75b3132bb9
Mohamed BASSAOUD <mohamed.bassaoud@yahoo.fr>
2020-02-11 17:14:22 +0100

    Commit de Momo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarouselDeMomoComponent } from './carousel-de-momo/carousel-de-momo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselDeMomoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/carousel-de-momo/carousel-de-momo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel-de-momo',
+  templateUrl: './carousel-de-momo.component.html',
+  styleUrls: ['./carousel-de-momo.component.scss']
+})
+export class CarouselDeMomoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
