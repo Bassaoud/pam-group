2e3dfbabc9c1bc3b55e68d17e4263d44fcad6020
Andrei <craciunescu.andrei@gmail.com>
2020-02-11 14:56:01 +0100

    top-navbar 0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopNavbarComponent } from './top-navbar/top-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavbarComponent
   ],
   imports: [
     BrowserModule
